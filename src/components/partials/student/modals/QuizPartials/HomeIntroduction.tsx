452e193088eb6b17ba17c590d2b033f456b23e02
Helmi Nur Ihsan <helmi.n.ihsan@gmail.com>
2023-08-20 15:05:13 +0700

    revisi

--- src/components/partials/student/modals/QuizPartials/HomeIntroduction.tsx
+++ src/components/partials/student/modals/QuizPartials/HomeIntroduction.tsx
@@ -34,11 +34,22 @@ export const HomeIntroductionModal = ({
                 Your browser does not support the video tag.
               </video>
               <p className="fs-4 fw-lighter my-2">
-                Gamphy Learning adalah sebuah platform game edukatif yang berisi
-                materi Gerak Lurus fisika tingkat SMA. Platform ini berisi
-                rangkuman materi dan beberapa latihan soal berupa challenge
-                dengan sistem level.
+                Gamphy Learning adalah sebuah{" "}
+                <span className="fst-italic">platform game</span> edukatif yang
+                berisi materi Gerak Lurus fisika untuk kelas X SMA.{" "}
+                <span className="fst-italic">Platform</span> ini berisi
+                rangkuman materi dan beberapa latihan soal berupa{" "}
+                <span className="fst-italic">challenge</span>&nbsp; dengan
+                sistem level. Sebelum mulai, kamu bisa lihat panduan penggunaan
+                Gamphy Learning dengan klik tombol di bawah ini.
               </p>
+              <a
+                href="bit.ly/GamphyLearningPanduan"
+                target="_blank"
+                className="btn btn-lg btn-primary"
+              >
+                Download Panduan
+              </a>
             </div>
             <div className="modal-footer justify-content-center">
               <button
